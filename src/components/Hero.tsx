463ceed0edb5fa3f1bc8503853696fa70cd39533
TangLang <langlang_1991@126.com>
2024-01-07 22:59:43 +0200

    Add email subscription functionality to Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,23 @@
+"use client";
+import { useState } from "react";
 import { ArrowRight } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { toast } from "sonner";
 
 export default function Hero() {
+  const [email, setEmail] = useState("");
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handleClick = () => {
+    toast("Thank you for subscribe!", {
+      description: `${email} has been added to the list.`,
+    });
+  };
+
   return (
     <>
       <div className="flex flex-col justify-start items-center">
@@ -11,8 +25,12 @@ export default function Hero() {
           芬兰博物馆参观指南
         </h1>
         <div className="flex w-full max-w-sm items-center space-x-2 mb-4">
-          <Input type="email" placeholder="输入邮箱" />
-          <Button type="submit">
+          <Input
+            type="email"
+            placeholder="输入邮箱"
+            onChange={handleEmailChange}
+          />
+          <Button type="submit" onClick={handleClick}>
             订阅
             <ArrowRight className="ml-2 h-4 w-4" />
           </Button>
