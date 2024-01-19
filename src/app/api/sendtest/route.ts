27346aa850eab4119b6fcd7a30b6f92fcd318fe4
TangLang <langlang_1991@126.com>
2024-01-19 14:29:00 +0200

    Remove unused code and update dependencies

--- src/app/api/sendtest/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  try {
-    return NextResponse.json({ "1": "world" });
-  } catch (error) {
-    return NextResponse.json({ error });
-  }
-}
--- src/app/api/speech/route.ts
+++
@@ -1,5 +0,0 @@
-import fs from "fs";
-import path from "path";
-import OpenAI from "openai";
-
-const openai = new OpenAI();
